edcda67ffcfac6436e447231211024d569b3afe6
Rajdeep Mandal <mrajdeep651@gmail.com>
2025-05-18 23:28:20 +0530

    Server

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Notification Service is running');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
